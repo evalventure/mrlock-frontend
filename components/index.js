7c324a2bf1feaace67ec632847c71d6b00735833
Alexandru Matei <alexandrumatei3693@gmail.com>
2022-01-14 23:01:18 +0200

    Add NextJS boilerplate

---
+++ components/index.js
@@ -0,0 +1,10 @@
+/**
+ * Here are the styles imported.
+ * Usage: <div className={s.myCustomClass} />
+ */
+import s from './s.module.css';
+
+export function Header() {
+  // TODO: Replace null with the HTML code for the header
+  return null;
+}
