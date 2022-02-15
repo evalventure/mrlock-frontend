654effbd4d436cdf686958610e986bd527403fbd
andreicatrina <Catrina.andrei93@yahoo.com>
2022-02-15 20:44:37 +0200

    update home page

--- components/Reviews/index.js
+++ components/Reviews/index.js
@@ -1,44 +1,45 @@
+import axios from "axios";
 import s from "./s.module.css";
 import { RatingStar } from "../Icons";
+import { useEffect, useState } from "react";
 
 export function Reviews() {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    getReviews();
+  }, []);
+
+  async function getReviews() {
+    const response = await axios.get(
+      "https://e4xwlli09l.execute-api.us-east-1.amazonaws.com/default/getBirminghamAutoLocksmithPageReviews"
+    );
+    setReviews(response.data.reviews);
+  }
+
   return (
     <section className={s.section}>
       <div className={s.container}>
         <div className={s.title}>WHAT PEOPLE SAY</div>
         <div className={s.reviewContainer}>
-          <div className={s.reviewCard}>
-            <p className={s.titleParagraph}>Sukhy Kaur Dyail </p>
-            <p className={s.date}>November 26 at 10:32</p>
-            <p className={s.description}>
-              Brilliant service from Mr Lock, top guy. Have used a few times now
-              and every time has done what’s needed efficiently and to a high
-              standard, would highly recommend A+++ service.
-            </p>
-            <div className={s.starContainer}>
-              <RatingStar />
-              <RatingStar />
-              <RatingStar />
-              <RatingStar />
-              <RatingStar />
-            </div>
-          </div>
-          <div className={s.reviewCard}>
-            <p className={s.titleParagraph}>Sukhy Kaur Dyail </p>
-            <p className={s.date}>November 26 at 10:32</p>
-            <p className={s.description}>
-              Brilliant service from Mr Lock, top guy. Have used a few times now
-              and every time has done what’s needed efficiently and to a high
-              standard, would highly recommend A+++ service.
-            </p>
-            <div className={s.starContainer}>
-              <RatingStar />
-              <RatingStar />
-              <RatingStar />
-              <RatingStar />
-              <RatingStar />
-            </div>
-          </div>
+          {reviews.map(function (review) {
+            return (
+              <div className={s.reviewCard}>
+                <p className={s.titleParagraph}>Sukhy Kaur Dyail </p>
+                <p className={s.date}>
+                  {new Date(review.created_time).toLocaleString()}
+                </p>
+                <p className={s.description}>{review.review_text}</p>
+                <div className={s.starContainer}>
+                  <RatingStar />
+                  <RatingStar />
+                  <RatingStar />
+                  <RatingStar />
+                  <RatingStar />
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
--- components/Services/index.js
+++ components/Services/index.js
@@ -2,6 +2,44 @@ import { useEffect, useState } from "react";
 import s from "./s.module.css";
 // import Image from "../images/backgroundImage.jpeg";
 
+const sliders = [
+  {
+    title: "Emergency car unlocking",
+    description:
+      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+  },
+  {
+    title: "Car key replacement",
+    description:
+      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+  },
+  {
+    title: "Transponder programming",
+    description:
+      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+  },
+  {
+    title: "Lost car key",
+    description:
+      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+  },
+  {
+    title: "Locked Key in the car",
+    description:
+      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+  },
+  {
+    title: "Key cutting physical keys",
+    description:
+      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+  },
+  {
+    title: "Broken key removal",
+    description:
+      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+  },
+];
+
 export function Services() {
   const [step, setStep] = useState(0);
   useEffect(() => {
@@ -28,19 +66,18 @@ export function Services() {
               The average arriving time is 30 min after your call.
             </p>
             <div className={s.sliders}>
-              {Array(7)
-                .fill(0)
-                .map((_, i) => {
-                  return (
-                    <div className={s.sliderStep}>
-                      <div
-                        className={`${s.segment} ${i === step ? s.active : ""}`}
-                      >
-                        <div></div>
-                      </div>
+              {sliders.map((sliderItem, i) => {
+                return (
+                  <div className={s.sliderStep}>
+                    <div
+                      className={`${s.segment} ${i === step ? s.active : ""}`}
+                    >
+                      <div></div>
                     </div>
-                  );
-                })}
+                    <p className={s.segmentTitle}>{sliderItem.title}</p>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
