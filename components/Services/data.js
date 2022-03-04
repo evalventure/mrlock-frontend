82bd823a757fbcf4f100a03911c56055e4951944
Alexandru Matei <alexandrumatei3693@gmail.com>
2022-03-04 23:40:54 +0200

    Update texts and fix bugs

--- components/Services/data.js
+++ components/Services/data.js
@@ -2,36 +2,36 @@ export const sliders = [
   {
     title: "Emergency car unlocking",
     description:
-      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+      "Our trained locksmiths deal with car lockouts on a daily basis. We will come to the car's location and open the doors or boot of any make or model of vehicle without damaging the car or the lock.",
   },
   {
     title: "Car key replacement",
     description:
-      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+      "If your key is broken or you're missing a spare we can provide new remote or standard keys in Birmingham and West Midlands.",
   },
   {
     title: "Transponder programming",
     description:
-      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+      "A transponder – short for 'transmitter and responder' – has been commonplace in a car's ignition key since the mid-1990s.",
   },
   {
     title: "Lost car key",
     description:
-      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+      "Our mobile key-cutting machinery is able to hand both laser key and traditional keys. This versatility means that we are able to help a wide variety of people at the side of the road so that you’ll hardly ever receive a negative prognosis from us!",
   },
   {
     title: "Locked Key in the car",
     description:
-      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+      "Don't worry, we've all done it at some point! Whether you left your keys on the dashboard before closing the door or you shut them in the boot, we can unlock your car and get them out for you, whether it’s a simple key, a transponder fob or a remote key.",
   },
   {
     title: "Key cutting physical keys",
     description:
-      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+      "When it comes to the key cutting services that we provide, we are always fully stocked with a range of keys that are ready to be cut.",
   },
   {
     title: "Broken key removal",
     description:
-      "We will get it back to you without any damages to your vehicle. The average arriving time is 30 min after your call.",
+      "There’s nothing more frustrating than being left with half a key in your hand when trying to unlock the door. This could happen anytime and for a number of reasons, such as the key has gone brittle over time, the door needs further adjustment, or you’ve simply applied too much pressure while turning the key.",
   },
 ];
