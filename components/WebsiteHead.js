e7978541d5049a2736c67b256bfc2d5da171386a
Alex Brebu <andrei.brebu13@gmail.com>
2022-04-20 13:43:14 +0300

    fix: unterminated html tags

--- components/WebsiteHead.js
+++ components/WebsiteHead.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 export function WebsiteHead() {
   return (
     <Head>
-      <html lang="en-GB">
       <title lang="en-GB">Mr Lock 24/7 | West Midlands Autolocksmith</title>
       <meta name="description" content="Your keys has been locked inside the car? No panic. Call us on 07979374288 and we will get it back to you without any vehicle damages." />
 
@@ -20,10 +19,10 @@ export function WebsiteHead() {
       <meta name="twitter:description" content="Your keys has been locked inside the car? No panic. Call us on 07979374288 and we will get it back to you without any vehicle damages." />
       <meta name="twitter:image" content="share-image.png" />
 
-      <meta name="language" content="en-GB">
-      <meta name="revisit-after" content="2 days">
-      <meta name="rating" content="GENERAL">
-      <meta name="robots" content="all">
+      <meta name="language" content="en-GB" />
+      <meta name="revisit-after" content="2 days" />
+      <meta name="rating" content="GENERAL" />
+      <meta name="robots" content="all" />
 
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
