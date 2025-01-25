e1abdb6956f497ec43336fbb196d33252d441303
Alex Brebu <alexandru.brebu@jove.co>
2025-01-25 18:56:35 +0200

    fix: update release workflow

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   // Ensure compatibility with older browsers if needed
   experimental: {
     forceSwcTransforms: true,
