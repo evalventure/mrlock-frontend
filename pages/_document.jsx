ec3c875d8a8f6ce856b9c1b4c496367951785460
Alex Brebu <alexandru.brebu@jove.co>
2025-01-25 18:22:40 +0200

    fix: update Google Tag Manager with latest id

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,7 @@ export default function Document() {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-K9ZHLKG');`}}></script>
+        })(window,document,'script','dataLayer','GTM-585VSPV4');`}}></script>
         {/* End Google Tag Manager */}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2137178551510754"
         crossOrigin="anonymous"></script>
@@ -29,7 +29,7 @@ export default function Document() {
       </Head>
       <body>
           {/* Google Tag Manager (noscript) */}
-          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K9ZHLKG"
+          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-585VSPV4"
           height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           {/* End Google Tag Manager (noscript) */}
         <Main />
